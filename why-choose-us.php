9103e69c6ccde7f3534ec8251626a1613f13b97e
Alex Fore <forealexander4@gmail.com>
2018-04-09 20:07:40 -0500

    Created HTML for why-choose-us.php

--- why-choose-us.php
+++ why-choose-us.php
@@ -5,7 +5,67 @@
 
 ?>
 
+    <section class="why-banner-container">
+        <div class="why-banner-img-wrapper"></div>
+        <div class="blue-box"></div>
+        <div class="banner-text"><h1>SUPERIOR<br>CLEANERS</h1></div>
+    </section> <!-- why-banner-container -->
 
+    <section class="excellence-container">
+        <div class="excellence-title">
+            <h3>EXCELLENCE<br>AND QUALITY</h3>
+        </div> <!-- excellence-title -->
+
+        <div class="excellence-text-container">
+            <div class="text-top-container">
+                <div class="client-container">
+                    <h4></h4>
+                    <p></p>
+                </div> <!-- client-container -->
+                <div class="green-container">
+                <h4></h4>
+                    <p></p>
+                </div> <!-- green-container --> 
+            </div> <!-- text-top-container -->
+
+            <div class="text-bottom-container">
+                <div class="account-container">
+                    <h4></h4>
+                    <p></p>
+                </div> <!-- account-container -->
+                <div class="value-container">
+                <h4></h4>
+                    <p></p>
+                </div> <!-- value-container --> 
+            </div> <!-- text-bottom-container -->
+
+        </div> <!-- excellence-text-container -->
+    </section> <!-- excellence-container -->
+
+    <section class="service-container">
+        <div class="service-text">
+            Superior service and customer satisfaction are everything at RamClean! Our entire commercial 
+            cleaning operation is designed to deliver ongoing professionalism and the finest cleaning 
+            service in the Champaign-Urbana area.
+        </div> <!-- service-text -->
+    </section> <!-- service-container -->
+
+    <section class="staff-container">
+        <div class="staff-text">
+            <p>All RamClean cleaning staff is bonded and insured, and their honesty and reliability are 
+            as important as their cleaning expertise.</p>
+            
+            <p>RamClean ownership is hands-on and local every day, working alongside highly skilled, 
+            licensed and bonded cleaning teams.  All RamClean clients have direct access to our owner 24/7</p>
+
+            <p>Our valued Champaign-Urbana clients know the RamClean difference. From construction 
+            cleanups to window cleaning and small-office cleaning, we’ve got every detail covered to ensure 
+            100 percent satisfaction.</p>
+        </div> <!-- staff-text -->
+        <div class="staff-img-container">
+            <img src="#" alt="Clean, modern office space with pops of orange" />
+        </div> <!-- staff-img-container -->
+    </section> <!-- staff-container -->
 
 
 
